964b7f7b43f7dbbeb95655663e214d63ee6fa8e9
cliorz <cliorz@icloud.com>
2024-04-14 14:17:48 -0400

    Update client.py

--- GUI_pyqt/client.py
+++ GUI_pyqt/client.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 class AudioServerClient:
     def __init__(self, url = "http://frodo.colby.edu:5000/chat", auth_key ="?Djk8;<jz:.~s*6Bn2dc>Y4TrT<P=E" , directory = ''):
@@ -6,36 +7,32 @@ class AudioServerClient:
         self.auth_key = auth_key
         self.directory = directory
 
-    def send_audio(self, audio_files):
+    def send_audio(self, audio_file):
         """
         Sends audio files to the server and prints the server's response.
 
-        :param audio_files: A list of audio file names to be sent.
+        :param audio_files: audio file names to be sent.
         """
-        for audio_file in audio_files:
-            audio_file_path = self.directory + audio_file
-            print('\n\nReading audio file')
-            # Read the audio file
-            with open(audio_file_path, "rb") as audio_dat:
-                audio_data = audio_dat.read()
-
-            # Send a POST request to the server
-            print('Sending request\n')
-            response = requests.post(
-                self.url,
-                headers={"Auth-Key": self.auth_key},
-                data=audio_data,
-                timeout=60
-            )
-            # Print the server's response
-            print('User:', response.json()['input'])
-            print('Assistant:', response.json()['response'])
         
-        #save the audio file
-        with open('response.wav', 'wb') as f:
-            f.write(response.content)
-        #return the text
-        return response.json()['response']
+        audio_file_path = self.directory + audio_file
+        print('\n\nReading audio file')
+        # Read the audio file
+        with open(audio_file_path, "rb") as audio_dat:
+            audio_data = audio_dat.read()
+            print('audio data:', audio_file_path)
+
+        # Send a POST request to the server
+        print('Sending request\n')
+        response = requests.post(
+            self.url,
+            headers={"Auth-Key": self.auth_key},
+            data=audio_data,
+            timeout=60)
+            # # Print the server's response
+        #print(json.dumps(response, indent=4)) 
+        print('User:', response.json()['input'])
+        print('Assistant:', response.json()['response'])
+        return response
 
 # Example usage
 if __name__ == "__main__":
@@ -46,7 +43,6 @@ if __name__ == "__main__":
     # Directory containing audio files
     directory = "Backend/AudioTests/"
     # List of audio files to send
-    audio_files = ["joke.m4a", "3body.m4a"]
 
     client = AudioServerClient(url, auth_key, directory)
-    client.send_audio(audio_files)
+    #client.send_audio(audio_files)
